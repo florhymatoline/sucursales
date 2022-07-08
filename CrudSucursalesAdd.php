cd10b3ae6509d5090d4cad7c2d52cbb2867ba70f
Simón Rossini <simonrossin@gmail.com>
2022-07-08 08:33:19 -0300

    modal que muestra info de usuarios(en proceso) se eliminó la columna de 'encargados'

--- CrudSucursalesAdd.php
+++ CrudSucursalesAdd.php
@@ -6,14 +6,14 @@ require 'conexion.php';
 $sucursal = $_POST['sucursal'];
 $localizacion = $_POST['localizacion'];
 $capacidad_max = $_POST['capacidad_max'];
-$encargado = $_POST['encargado'];
+//$encargado = $_POST['encargado'];
 $id_user = $_POST['id_user'];
 
 
 
 
 
-$q = "INSERT INTO sucursales (`sucursal`, `localizacion`, `capacidad_max`, `encargado`, `id_user`) VALUES ('$sucursal','$localizacion','$capacidad_max','$encargado','$id_user')";
+$q = "INSERT INTO sucursales (`sucursal`, `localizacion`, `capacidad_max`, `encargado`, `id_user`) VALUES ('$sucursal','$localizacion','$capacidad_max','','$id_user')";
 
 echo mysqli_query($con, $q);
 
