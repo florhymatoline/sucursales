a28a36facfaf46dae3a74e0109b6d4bb94e44e35
Simón Rossini <simonrossin@gmail.com>
2022-07-08 06:42:40 -0300

    id de usuario ahor se selecciona con un "select"

---
+++ ConsultaId.php
@@ -0,0 +1,31 @@
+<?php
+
+require 'conexion.php';
+
+
+#$sucursal = $_POST['id'];
+
+
+
+$q = "SELECT  id_user FROM usuarios";
+
+$r = mysqli_query($con, $q);
+
+if (mysqli_num_rows($r) > 0){
+  
+    while ($valores = mysqli_fetch_assoc($r)){
+       $array[]= $valores;
+       //echo($array[1]);
+      }
+      //envio respuesta a ajax como json
+
+      echo (json_encode($array)); 
+      
+      header('Content-Type: application/json');
+      //↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑       
+      //Función necesaria para que php envie el array correctamente a ajax
+    }
+
+mysqli_close($con);
+
+?>
