9a65af471a2aa4b2eb6a67c2e7fb4a2ca3e457bc
Florencia Hymatoline <hymatolineflorencia@gmail.com>
2022-07-08 21:28:49 -0300

    cruds

--- Sailor/agregar-s.php
+++ Sailor/agregar-s.php
@@ -1,9 +1,9 @@
 <?php
 
-$su= $_POST['sucursal'];
-$l= $_POST['localizacion'];
-$cp= $_POST['capacidad_max'];
-$us= $_POST['id_user'];
+$su= $_POST['sucursal1'];
+$l= $_POST['localizacion1'];
+$cp= $_POST['capacidad_max1'];
+$us= $_POST['id_user1'];
 
 require 'conexion.php';
 $q="INSERT INTO sucursales (sucursal,localizacion,capacidad_max,id_user)VALUES ('$su','$l','$cp','$us')";
--- Sailor/editar.php
+++ Sailor/editar.php
@@ -8,7 +8,7 @@ $p= MD5($_POST['contra']);
 $tp= $_POST['tipo_user']; 
 $oc= $_POST['ocupacion']; 
 
-$q="UPDATE usuarios SET id_user='$id', nmb_comp='$nm', email='$em', contra='$p', tipo_user='$tp', ocupacion='$oc' WHERE id_user='$id'";
+$q="UPDATE usuarios SET nmb_comp='$nm', email='$em', contra='$p', tipo_user='$tp', ocupacion='$oc' WHERE id_user='$id'";
 
 $r=mysqli_query($con, $q);
 
