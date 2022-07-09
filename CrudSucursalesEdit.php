490ce9f66b28f9ab14b0488d802e8fd9e16ced0a
Simón Rossini <simonrossin@gmail.com>
2022-07-08 21:30:05 -0300

    .

--- CrudSucursalesEdit.php
+++ CrudSucursalesEdit.php
@@ -5,7 +5,7 @@ $id_sucursal = $_POST['id_sucursalEdit'];
 $sucursalEdit = $_POST['sucursalEdit'];
 $localizacion = $_POST['localizacionEdit'];
 $capacidad_max = $_POST['capacidad_maxEdit'];
-$encargado = $_POST['encargadoEdit'];
+#$encargado = $_POST['encargadoEdit'];
 $id_user = $_POST['id_userEdit'];
 
 
@@ -13,9 +13,8 @@ $id_user = $_POST['id_userEdit'];
 
 
 
-$q = "UPDATE sucursales SET sucursal='$sucursalEdit',localizacion='$localizacion',capacidad_max='$capacidad_max',encargado='$encargado',id_user='$id_user' WHERE id_sucursal = '$id_sucursal'";
+$q = "UPDATE sucursales SET sucursal='$sucursalEdit',localizacion='$localizacion',capacidad_max='$capacidad_max',id_user='$id_user' WHERE id_sucursal = '$id_sucursal'";
 
 echo mysqli_query($con, $q);
-
 
 ?>
