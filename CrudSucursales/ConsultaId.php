3ef50fd1c016df3d04e73ce778b9b9eb1250d323
Simón Rossini <simonrossin@gmail.com>
2022-07-11 17:01:26 -0300

    los select hora muestran el nombre en relacion al id

--- CrudSucursales/ConsultaId.php
+++ CrudSucursales/ConsultaId.php
@@ -7,7 +7,7 @@ require 'conexion.php';
 
 
 
-$q = "SELECT id_user FROM usuarios WHERE ocupado = 'NO'";
+$q = "SELECT id_user,nmb_comp FROM usuarios WHERE ocupado = 'NO'";
 
 $r = mysqli_query($con, $q);
 
